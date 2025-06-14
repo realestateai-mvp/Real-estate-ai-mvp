a34b968cbc719b07de855322fd4ae8a2558d8ed8
realestateai-mvp <bradd123@icloud.com>
2025-06-14 20:25:34 +0100

    Update Real_estate_ai.py

--- Real_estate_ai.py
+++ Real_estate_ai.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Real estate backend live"}
+async def root():
+    return {"message": "Hello from FastAPI!"}
